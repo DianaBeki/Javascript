d2929f52f49de8bf004e7a0144699f4fd8fa1621
Diana Beki <dianabeki54@gmail.com>
2023-01-30 21:01:44 +0300

    Using add function

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let number = 11;
   }
 }
 
-numberChecker();*/
+numberChecker();
 
 const birthYear = 1948;
 const thisYear = 1965;
@@ -19,3 +19,49 @@ const lastName = "Beki";
 const greeting = "Hello! My name is " + firstName" + " " + lastName + " and I am " + (thisYear - birthYear) + " years onload.";
 
 console.log(greeting);
+
+function greet (name = Diana) {
+
+}
+
+const Name = 'Zaptec';
+
+function greeting() {
+  alert(`Our company is called ${name}.`);
+
+
+  function a() {
+    const x = 1;
+  }
+
+  function b() {
+    const z = 3;
+  }
+
+  function output(value) {
+
+  }const para = document.createElement('p');
+  document.body.appendChild(para);
+  para.text) = `Value: ${value}`;
+}
+
+console.log(output())
+
+
+function favoriteAnimal(animal) {
+  return animal + " is my favorite Animal! "
+}
+
+console.log(favoriteAnimal('Goat'))
+*/
+
+
+function add7(sum) {
+  
+  sum = 2 + 7; 
+
+
+}
+
+console.log(sum);
+add7(sum);
