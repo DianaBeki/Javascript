0cc2de5a0cb73d9dd569925d871bf69a29523684
Diana Beki <dianabeki54@gmail.com>
2023-01-30 13:19:22 +0300

    Use of if else statement

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Let the code return true when the number is greater than or equal to 10, and fALSE if it is less than 10
 
-function numberChecker() {
+/*function numberChecker() {
 let number = 11; 
   if(number <= 10) {
     return true;
@@ -9,4 +9,13 @@ let number = 11;
   }
 }
 
-numberChecker();
+numberChecker();*/
+
+const birthYear = 1948;
+const thisYear = 1965;
+const firstName = "Diana";
+const lastName = "Beki";
+
+const greeting = "Hello! My name is " + firstName" + " " + lastName + " and I am " + (thisYear - birthYear) + " years onload.";
+
+console.log(greeting);
