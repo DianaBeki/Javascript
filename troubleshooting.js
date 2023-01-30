226b2db2fefa4d9cab8f09266cef87b419ceccef
Diana Beki <dianabeki54@gmail.com>
2023-01-30 09:13:25 +0300

    Convert strings to numbers

---
+++ troubleshooting.js
@@ -0,0 +1,13 @@
+ function troubleshooting () {
+  
+  const a = +2;
+  const b = +3;
+
+  let result = "a" + "b";
+
+  return result;
+ }
+
+ troubleshooting();
+
+ //The unary + operator is used to convert a variable to a number.
