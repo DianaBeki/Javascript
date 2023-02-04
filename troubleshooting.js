d38d8eff892768973ba2eef61327ebfad703a56c
Diana Beki <dianabeki54@gmail.com>
2023-02-04 14:13:23 +0300

    Manipulating DOM

--- troubleshooting.js
+++ troubleshooting.js
@@ -11,6 +11,19 @@
 
  troubleshooting();
 
+//  const div = document.createElement('div'); 
+//  Editing attributes
+//   div.setAttribute('id', 'theDiv' ) //if id exists,update it to 'theDiv, else create an id with value "theDiv'
+ 
+//   div.getAttribute('id'); //returns value of specified attribute, in this case 'theDiv'.
+ 
+//   div.removeAttribute('id'); //removes specified attribute
 
+//  Working With Classes
+//div.ClassList.add('new'); -adds class "new" to your new div.
+
+//div.ClassList.remove('new'); - removes 'new' class from div.
+
+//div.ClassList.toggle('active'); -if div doesn't have class active then add it, or if it does, then remove it.
 
  //The unary + operator is used to convert a variable to a number.
