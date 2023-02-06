5a9aae700bd46c16d5b42369ba958c288dff05c7
Diana Beki <dianabeki54@gmail.com>
2023-02-06 17:45:31 +0300

    DOM manipulation

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const body = document.body
-const div = document.querySelector("div")
-const spanHi = document.querySelector("#hi")
-const spanBye = document.querySelector("Bye")
-const div = document.createElement('div');
-div.innerText = "Hello World"
-body.append(div)
 
 const div = document.querySelector("div")
+
+const spanHi = document.querySelector("#hi")
+
+const spanBye = document.querySelector("Bye")
+
+
+// const div = document.createElement('div');
+// div.innerText = "Hello World"
+// body.append(div)
+
+// const div = document.querySelector("div")
