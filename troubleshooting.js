fae1ec1ad53b5c4588d88c05ec38e9b7d41242a9
Diana Beki <dianabeki54@gmail.com>
2023-02-09 14:41:57 +0300

    Ternary operator

--- troubleshooting.js
+++ troubleshooting.js
@@ -506,3 +506,13 @@
 // `Adding ${num1} and ${num2} gives ${num1 + num2}.`;
 // // => Adding 1 and 2 gives 3.
 // In the example above, backticks - (``) - are used to represent a template string. The${...} is the placeholder that is used for substitution. Any non-string types are implicitly converted to strings. This topic is covered in the type conversion concept. All types of expressions can be used with template strings.
+
+About Ternary Operator
+The conditional ternary operator is used to write a condensed expression that returns one of two alternate values based on some condition. It is often referred to as the "ternary operator". The name stems from the fact that the operator has three operands: predicate ? consequent-expression : alternative-expression It can be used as a replacement for short if-else statements.
+
+Similar to if statements, JavaScript will perform implicit type conversion to evaluate the condition. If the condition is truthy, the operand on the left-hand side of the colon will be returned. Otherwise the result of the ternary expression is the operand on the right-hand side of the colon.
+
+const year = 2020;
+
+year > 2000 ? 'in the past years' : 'a long time ago';
+// => 'in the past years'
